1246c9076d19f7d5d04b47565754b863d9349538
root9B <eliteentertainmentgaming@gmail.com>
2024-11-02 13:36:56 +0000

    Used 'Core/Log.h' in Application

--- Echo/src/Echo/Core/Application.cpp
+++ Echo/src/Echo/Core/Application.cpp
@@ -1,4 +1,5 @@
 #include "Application.h"
+#include "Log.h"
 #include <iostream>
 
 namespace Echo {
@@ -13,19 +14,20 @@ namespace Echo {
 
     void Application::Run() {
         while (m_Running) {
-            std::cout << "Application is running..." << std::endl;
+            ECHO_CORE_TRACE("Application is running...");
 
             char input;
             std::cout << "Press 'q' to quit: ";
             std::cin >> input;
             if (input == 'q') {
                 m_Running = false;
+                ECHO_CORE_INFO("Application is stopping...");
             }
         }
     }
 
     Application* CreateApplication() {
-        return new Application();
+        return new Application(); // Adjust if you have a derived class of Application
     }
 
 } // namespace Echo
